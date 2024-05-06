c180df6c65147587886407624f45b478dc77ac1e
dixsonhuie <dixson.huie@gmail.com>
2024-05-06 16:25:04 -0400

    Create ganymedeinstall.sh

---
+++ jupyter-setup/ganymedeinstall.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+JAVA_HOME="/usr/lib/jvm/java-11-openjdk-amd64"
+
+# Example basic install command that we will expand upon
+# java -jar ganymede-2.1.2.20230910.jar -i
+
+GANYMEDE_JAR="ganymede-2.1.2.20230910.jar"
+
+$JAVA_HOME/bin/java \
+      -Dcom.gs.jini_lus.locators="localhost" \
+      -jar $GANYMEDE_JAR \
+      -i \
+      --id-suffix=gs-dev-lab-1.0 --display-name-suffix="with gs-dev-lab 1.0"
+
+# run jupyter notebook from the same directory where gs-dev-jupyter-lab is located
