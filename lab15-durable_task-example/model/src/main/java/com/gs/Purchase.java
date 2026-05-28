b7345ae2576746e8379718dd883840e82331aaf8
Dixson Huie <dixson.huie@gmail.com>
2026-05-28 17:00:07 -0400

    update_durable_task_example

---
+++ lab15-durable_task-example/model/src/main/java/com/gs/Purchase.java
@@ -0,0 +1,76 @@
+package com.gs;
+
+import com.gigaspaces.annotation.pojo.SpaceClass;
+import com.gigaspaces.annotation.pojo.SpaceId;
+import com.gigaspaces.annotation.pojo.SpaceIndex;
+import com.gigaspaces.metadata.index.SpaceIndexType;
+
+
+@SpaceClass
+public class Purchase {
+    private String id;
+    private Integer amount;
+    private Integer customerId;
+    private Integer productId;
+    private PurchaseStatus purchaseStatus = PurchaseStatus.NEW;
+
+
+    public Purchase() {
+    }
+
+    public Purchase(Integer amount, Integer customerId, Integer productId) {
+        this.amount = amount;
+        this.customerId = customerId;
+        this.productId = productId;
+    }
+
+    public Purchase(PurchaseStatus purchaseStatus) {
+        this.purchaseStatus = purchaseStatus;
+    }
+
+    public PurchaseStatus getPurchaseStatus() {
+        return purchaseStatus;
+    }
+
+    public void setPurchaseStatus(PurchaseStatus purchaseStatus) {
+        this.purchaseStatus = purchaseStatus;
+    }
+
+    @SpaceId(autoGenerate = true)
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @SpaceIndex(type = SpaceIndexType.EQUAL_AND_ORDERED)
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+
+    public Integer getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Integer customerId) {
+        this.customerId = customerId;
+    }
+
+
+    public Integer getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Integer productId) {
+        this.productId = productId;
+    }
+
+
+}
